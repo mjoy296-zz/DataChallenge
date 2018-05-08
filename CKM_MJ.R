084202f8d5b9bbcd30ed7d96914eef82a06aa06b
mjoy <mjoy2961@gmail.com>
2018-05-08 17:13:20 -0400

    update

--- CKM_MJ.R
+++ CKM_MJ.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(anytime)
+library(forecast)
 
 options(scipen = 999)
 
@@ -11,13 +12,20 @@ comps = read.csv('./CompData.csv')
 nrow(comps)
 head(comps)
 
-#convert change_ts to timestamps
-comps$change_ts <- substr(comps$change_ts, 1, nchar(comps$change_ts) - 3)
-comps$change_ts <- as.numeric(comps$change_ts)
-comps$change_ts <- anytime(comps$change_ts)
+is.ts(comps$change_ts)
+#start(comps$change_ts)
+#end(comps$change_ts)
+
+#frequency(comps$change_ts)
+
+#autoplot(comps$change_ts, facets = FALSE)
+
+#comps$change_ts <- msts(comps$change_ts, start = 1514764800000, frequency = 24)
+
+is.ts(comps$change_ts)
 
 
-# comps$component_name <- gsub('COMPONENT', "",x = comps$component_name)
+ # comps$component_name <- gsub('COMPONENT', "",x = comps$component_name)
 # comps$component_name <- gsub("(^|[^0-9])0+", "\\1", comps$component_name) 
 comps$component_name <- as.character(comps$component_name)
 
@@ -40,7 +48,7 @@ target4 <- c("B57X3_03_00", "29MSJ_03_01", "7EFLOP_03_02")
 target5 <- "assembly room"
 
 
-#get rate of ouput
+#get rate of ouput per component 
 roo <- ordered %>%
   filter(., location %in% target) %>%
   group_by(., component_type, location) %>%
@@ -57,54 +65,76 @@ rates <- roo %>%
 rates
 ##component 3 has worst rate of output at 5.23%
 
-#filter by starting and final location to get difference
-times = ordered1 %>%
-  group_by(component_type) %>%
-  filter(., location %in% target) 
 
-times2 = as.data.frame(times)
-times2$component_type <- NULL
-times2$location <- NULL
-#times2 = times2 %>% tibble::rowid_to_column(., "ID")
-times2 <- times2[!duplicated(times2), ]
-colSums(is.na(times2))
+# #convert change_ts to timestamps
+comps$times <- as.numeric(substr(comps$change_ts, 1, nchar(comps$change_ts) - 3))
+comps$times <- anytime(comps$times)
 
-#spread to get status as columns to get difference btw first and final
-r <- times2 %>% 
+#get average time it takes specific component to 
+#go from first machine to assembly room 
+name_diff <- comps %>%
   group_by(., component_name) %>%
-  spread(., status, change_ts) %>%
-  mutate(., tot_time = as.numeric(difftime(complete, `in progress`)))
+  mutate(., diff = as.numeric(difftime(max(times), min(times))))
 
-sum(is.na(r$tot_time))
+diffs <- name_diff %>%
+  group_by(., component_type) %>%
+  summarise(., avg = mean(diff))
+diffs
+#ZZU3X_02_00 is the only machine that is shared by 0 and 3
+#so it seems like there is a slow down there 
 
-r$complete <- NULL
-r$`in progress` <- NULL
 
-comp = comps[, c('component_name', "component_type")]
+l <- comps %>%
+  select(., component_name, component_type, location, times) %>%
+  arrange(., component_name)
 
-diff <- merge(comp, r, by = "component_name", all.x = TRUE)
-diff = diff[!duplicated(diff),]
-diff$component_name <- NULL
+dput(head(l, 13))
+
+
+
+loc_diff <- comps %>%
+          group_by(., component_type, location) %>%
+          mutate(., diff = as.numeric(difftime(max(times), min(times))))
+ld <- loc_diff %>%
+      group_by(., location) %>%
+      summarise(., avg = mean(diff))
+
+
+dts <- decompose(comps$change_ts, type = "additive")
+plot(dts)
+stlRes <- stl(comps$change_ts, s.window = "periodic")
+stlRes
+
+
+
+
+#dataset separated by each component
+comp0 <- comps %>%
+  filter(., component_type == "component_0")
+
+comp1 <- comps %>%
+  filter(., component_type == "component_1")
+
+comp2 <- comps %>%
+  filter(., component_type == "component_2")
+
+comp3 <- comps %>%
+  filter(., component_type == "component_3")
 
 colSums(is.na(diff))
 
-diff_g <- diff %>%
-            group_by(., component_type) %>%
-            summarise(., avg = mean(as.numeric(tot_time), na.rm = TRUE))
 
-times %>% group_by(., component_type) %>%
-  mutate()
-
-countbyloc = ordered %>%
+#view how many compoenents go through each location
+cl_ct = ordered %>%
   group_by(., component_type, location) %>%
   count(., component_type)
-
-
 
 ggplot(cl_ct, aes(component_type, n)) + 
   geom_bar(aes(fill = location), position = "dodge", stat = "identity")
 
+cl <- cl_ct %>% spread(., location, n)
 
+#view how many waiting/in progress/complete components at each location
 loc_stat = ordered %>%
     group_by(., location) %>%
     count(., status)
@@ -112,6 +142,7 @@ loc_stat = ordered %>%
 ggplot(loc_stat, aes(location, n)) + 
   geom_bar(aes(fill = status), position = "dodge", stat = "identity")
 
+ls <- loc_stat %>% spread(., status, n)
 
 status_count <- comps %>%
   group_by(., component_type) %>%
